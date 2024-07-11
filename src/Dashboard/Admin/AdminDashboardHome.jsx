0c77d9593de29abad1b64df376de4abcc5c5986f
rasc3ta <greenfair.shan5@gmail.com>
2024-07-11 21:33:37 +0600

    added stats for admin dashboard home page

--- src/Dashboard/Admin/AdminDashboardHome.jsx
+++ src/Dashboard/Admin/AdminDashboardHome.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { myAxios } from "../../Axios.config";
+import { FaUser } from "react-icons/fa6";
+import { MdOutlineAttachMoney } from "react-icons/md";
+import { BiSolidDonateBlood } from "react-icons/bi";
 
 const AdminDashboardHome = ({ user }) => {
   const [userCount, setUserCount] = useState(0);
+  const [requestCount, setRequestCount] = useState(0);
 
   useEffect(() => {
     myAxios
       .get("/userCount")
       .then((res) => setUserCount(res.data))
       .catch((e) => console.log(e.message));
+      
+    myAxios
+      .get("/requestCount")
+      .then((res) => setRequestCount(res.data))
+      .catch((e) => console.log(e.message));
+
   }, []);
 
   return (
@@ -18,75 +28,38 @@ const AdminDashboardHome = ({ user }) => {
         Welcome {user.name}
         {/* {console.log(latestThree)} */}
       </h1>
-      <div className=" flex justify-center">
-        <div className="stats shadow bg-crimson border-[4px] border-white">
-          <div className="stat text-white">
-            <div className="stat-figure ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title text-white min-h-[50px] text-2xl">
-              Total Users
-            </div>
-            <div className="stat-value ">{userCount}</div>
-            <div className="stat-desc text-white">21% more than last month</div>
-          </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 p-3  bg-crimson ">
+        <div className=" min-w-[200px]  border-[4px] border-white text-white flex flex-col items-center p-2 text-center justify-center">
+          <h3 className="text-3xl">Total Users : </h3>
+          <h3 className="text-4xl font-bold flex flex-row items-center gap-3 ">
+            {" "}
+            <span className="text-4xl ">
+              <FaUser />
+            </span>
+            <span className="text-4xl ">{userCount} </span>
+          </h3>
+        </div>
 
-          <div className="stat  bg-crimson border-[4px] border-white">
-            <div className="stat-figure text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 10V3L4 14h7v7l9-11h-7z"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title text-white min-h-[50px]  text-2xl">
-              Total Fundings
-            </div>
-            <div className="stat-value text-white">2.6M</div>
-            <div className="stat-desc text-white">21% more than last month</div>
-          </div>
-          <div className="stat  bg-crimson border-[4px] border-white">
-            <div className="stat-figure text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 10V3L4 14h7v7l9-11h-7z"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title text-white min-h-[50px]  text-2xl">
-              Total Blood <br /> Donation Requests
-            </div>
-            <div className="stat-value text-white">2.6M</div>
-            <div className="stat-desc text-white">21% more than last month</div>
-          </div>
+        <div className=" min-w-[200px] border-y-[4px] border-[4px] border-white text-white flex flex-col items-center p-2 text-center justify-center">
+          <h3 className="text-3xl">Total Fundings : </h3>
+          <h3 className="text-4xl font-bold flex flex-row items-center  ">
+            {" "}
+            <span className="text-5xl ">
+              <MdOutlineAttachMoney />
+            </span>
+            <span className="text-4xl ">{0} </span>
+          </h3>
+        </div>
+
+        <div className=" min-w-[200px]  border-[4px] border-white text-white flex flex-col items-center p-2 text-center justify-center">
+          <h3 className="text-3xl">Donation Requests : </h3>
+          <h3 className="text-4xl font-bold flex flex-row items-center gap-3 ">
+            {" "}
+            <span className="text-5xl ">
+              <BiSolidDonateBlood />
+            </span>
+            <span className="text-4xl ">{requestCount} </span>
+          </h3>
         </div>
       </div>
     </div>
@@ -94,7 +67,7 @@ const AdminDashboardHome = ({ user }) => {
 };
 
 AdminDashboardHome.propTypes = {
-  user: PropTypes.object,
+  user : PropTypes.object,
 };
 
 export default AdminDashboardHome;
